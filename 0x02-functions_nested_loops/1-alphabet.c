9fd6b4f6d8d7f862e78a7f69211eb8fc1d19e0e8
Abrahaley Tadesse <abrahaley.tadess@gmail.com>
2022-09-13 11:54:02 -0700

    lower case alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print lowercase alphabets
+ *
+ * Return: always 0
+ */
+void print_alphabet(void);
+{
+	int n;
+	for(n=97, n<123, n=n+1)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
