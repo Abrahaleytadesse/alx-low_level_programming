ebc64045b64f487b07a795765764f937ffaf8ddc
Abrahaley Tadesse <abrahaley.tadess@gmail.com>
2022-09-15 00:50:33 -0700

    check for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase characters
+ * @c : is thecharacter in ASCII code
+ *
+ * Return: on success 1(uppercase) otherwise 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else 
+	{
+		retrun (0);
+	}
+	_putchar('\n')
+}	
