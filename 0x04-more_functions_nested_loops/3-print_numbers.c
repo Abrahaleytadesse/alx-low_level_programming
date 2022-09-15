253084aea0eefe61fcbc7f35b4c0dc1d3ebf0303
Abrahaley Tadesse <abrahaley.tadess@gmail.com>
2022-09-15 02:00:56 -0700

    prints numbers 09

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 upto 9
+ *
+ * return : void
+ */
+void print_numbers(void)
+{
+	int num;
+	for (num = 48; num < 58; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
+
