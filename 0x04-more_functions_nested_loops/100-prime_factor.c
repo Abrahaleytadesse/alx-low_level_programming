b4f16efdf49cc3b1d8017c2006a8ab6e47736228
Abrahaley Tadesse <abrahaley.tadess@gmail.com>
2022-09-15 11:30:31 -0700

    prime factorization

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio>
+
+/**
+ * main - prints prime factor of a number.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int n, pf;
+
+	n = 612852475143;
+	for (pf = 2; pf <= n; pf++)
+	{
+		while(n / pf ==0)
+		{
+			printf("%ld", pf);
+			n = n / pf;
+		}
+	}
+	printf("\n");
+	return (0);
+}
+
+
