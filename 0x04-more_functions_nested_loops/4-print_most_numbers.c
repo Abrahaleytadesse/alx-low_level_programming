57bdfef2738e1c6460c3d2f836368192707bedd8
Abrahaleytadesse <108014323+Abrahaleytadesse@users.noreply.github.com>
2022-09-15 08:36:23 -0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,12 @@
  */
 void print_most_numbers(void)
 {
-	int ch;
+	int n;
 
-	for (ch = 48; ch < 58; ch++)
+	for (n = 48; n < 58; n++)
 	{
-		if (ch != 50 && ch != 52)
-			_putchar(ch);
+		if (n != 50 && n != 52)
+			_putchar(n);
 	}
 	_putchar('\n');
 }
