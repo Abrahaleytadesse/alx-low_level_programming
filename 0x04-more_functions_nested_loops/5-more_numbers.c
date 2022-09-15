a58290513b04f2b62defb0afaa9376f3ac52a6bd
Abrahaley Tadesse <abrahaley.tadess@gmail.com>
2022-09-15 05:13:28 -0700

    0 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14 ten times.
+ *Return: no return.
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+				_putchar((i / 10) + 48);
+			_putchar((i % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
