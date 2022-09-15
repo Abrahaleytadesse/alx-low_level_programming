4a42afe57e3fd1a8eac0104cea6f44fba98a23df
Abrahaley Tadesse <abrahaley.tadess@gmail.com>
2022-09-15 09:47:41 -0700

    prints triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle of hashes.
+ * @size: size of the triangle.
+ * Return: no return.
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size - i); j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 0; k <= i; k++)
+		{
+			_putchar('#');
+		}
+		if ( i < (size -1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
