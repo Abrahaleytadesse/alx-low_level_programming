a5437eca6502aa166b0b10070d68afe595fa3607
Abrahaleytadesse <108014323+Abrahaleytadesse@users.noreply.github.com>
2022-09-15 06:51:18 -0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,15 @@
 
 /**
  * print_numbers - prints numbers from 0 upto 9
- *
- * return : void
+ * return: no return 
  */
 void print_numbers(void)
 {
 	int num;
+	
 	for (num = 48; num < 58; num++)
 	{
 		_putchar(num);
 	}
 	_putchar('\n');
 }
-
