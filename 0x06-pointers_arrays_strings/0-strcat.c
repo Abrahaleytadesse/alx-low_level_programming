d8b2f0d4bec20e1d055355a5e10ab3a443b322ce
Abrahaley Tadesse <abrahaley.tadess@gmail.com>
2022-09-22 11:58:48 -0700

    concatenating

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,14 +11,14 @@ char *_strcat(char *dest, char *src)
 	int len1 = 0;
 	int len2 = 0;
 
-	while (*(dest + len1) != \'0')
+	while (*(dest + len1) != '\0')
 	{
 		len1 = len1 + 1;
 	}
 	while (len2 >= 0)
 	{
 		*(dest + len1) = *(src + len2)
-		if (*(src + len2) == \'0')
+		if (*(src + len2) == '\0')
 			break;
 		len1 = len1 + 1;
 		len2 = len2 + 1;
